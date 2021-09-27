646f5e984e84ed8729e8d8e9e6dfcec8fa31fc2d
Marcello Lima <araujodba@gmail.com>
2021-09-27 13:15:26 -0300

    Build Book Detail View.

---
+++ Lesson 15/Views/BookRatingView.swift
@@ -0,0 +1,71 @@
+//
+//  BookRatingView.swift
+//  Lesson 15
+//
+//  Created by Marcello Lima on 27/09/21.
+//
+
+import SwiftUI
+
+struct BookRatingView: View {
+    
+    @State var book: Book
+
+    var body: some View {
+        
+        VStack (alignment:. leading) {
+            Text(book.title)
+                .font(.largeTitle)
+                .bold()
+                .padding(.bottom, 20)
+                
+            
+
+            VStack (alignment: .center) {
+                
+                Text("Read Now!").font(.title)
+
+                Image(book.image!)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top, 5)
+                    .padding(.bottom, 25)
+            
+                
+                    Text("Mark for later:")
+                        .bold()
+                        .padding(.bottom, 10)
+                       
+                        
+                        
+                Image(systemName: book.isFavourite ? "star.fill": "star")
+                        .renderingMode(.original)
+                        .font(.system(size: 30, weight: .medium))
+                        .padding(.bottom, 30)
+                        .onTapGesture {
+                            book.isFavourite = !book.isFavourite
+                        }
+                
+                
+               
+                Text("Rate \(book.title)")
+                        .bold()
+                    Picker("", selection: $book.rating) {
+                        Text("1").tag(1)
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                        Text("5").tag(5)
+                    }.pickerStyle(.segmented)
+                    .padding(.horizontal)
+                        
+            }
+        }.padding(30)
+    }
+}
+
+struct BookRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookRatingView(book: BookModel().books[0])
+    }
+}
--- Lesson 15/Views/ContentView.swift
+++ Lesson 15/Views/ContentView.swift
@@ -12,27 +12,27 @@ struct ContentView: View {
     @ObservedObject var model: BookModel = BookModel()
     
     var body: some View {
-    GeometryReader { geo in
-        NavigationView {
-            ScrollView {
-                VStack {
-                    ForEach (model.books) { item in
+        GeometryReader { geo in
+            NavigationView {
+                ScrollView {
+                    VStack {
+                        ForEach (model.books) { item in
                         
-                                ZStack (alignment:.topLeading) {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .padding()
-                                        .shadow(color: Color.black.opacity(0.5), radius: 10, x: -4, y: 8)
-                                        .shadow(color: Color.black.opacity(0.3), radius: 5, x: 1, y: 1)
+                            ZStack (alignment:.topLeading) {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .padding()
+                                    .shadow(color: Color.black.opacity(0.5), radius: 10, x: -4, y: 8)
+                                    .shadow(color: Color.black.opacity(0.3), radius: 5, x: 1, y: 1)
                                     .foregroundColor(.white)
                                     
-                                    VStack (alignment:.leading) {
-                                        Spacer()
-                                        Text(item.title).font(.title).bold()
-                                        Spacer()
-                                        Text(item.author).font(.title2).italic()
-                                        Image(item.image!).resizable().scaledToFit()
-                                    }.padding(40)
-                                }.frame(width: geo.size.width, height: 700, alignment: .top)
+                                VStack (alignment:.leading) {
+                                    Spacer()
+                                    Text(item.title).font(.title).bold()
+                                    Spacer()
+                                    Text(item.author).font(.title2).italic()
+                                    Image(item.image!).resizable().scaledToFit()
+                                }.padding(40)
+                            }.frame(width: geo.size.width, height: 700, alignment: .top)
                         }
                     }
                 }.navigationTitle("My Library")
